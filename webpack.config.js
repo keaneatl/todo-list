06f4693b87944bcf38c496594fe68423fed64f28
usrwatermelon <kyntltntn@protonmail.com>
2021-10-08 14:35:08 +0000

    Added Babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,19 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
     ],
   },
 };
